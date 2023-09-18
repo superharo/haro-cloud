5c107d2d0b5ad1aa45d77291b0948c85de99a7dd
haro <superh.sun@hi-finance.com.cn>
2023-09-18 15:23:59 +0800

    redis框架组件

---
+++ haro-framework/haro-spring-boot-starter-redis/src/main/java/online/superh/haro/framework/redis/config/HaroCacheAutoConfiguration.java
@@ -0,0 +1,69 @@
+package online.superh.haro.framework.redis.config;
+
+import cn.hutool.core.util.StrUtil;
+import online.superh.haro.framework.redis.core.TimeoutRedisCacheManager;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.util.Objects;
+
+import static online.superh.haro.framework.redis.config.HaroRedisAutoConfiguration.buildRedisSerializer;
+
+/**
+ * @version: 1.0
+ * @author: haro
+ * @description:
+ *          Cache 配置类，基于 Redis 实现
+ * @date: 2023-09-14 16:28
+ */
+@AutoConfiguration
+@EnableConfigurationProperties({CacheProperties.class})
+@EnableCaching
+public class HaroCacheAutoConfiguration {
+
+    @Bean
+    @Primary
+    public RedisCacheConfiguration redisCacheConfiguration(CacheProperties cacheProperties) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+        // 设置使用 : 单冒号，而不是双 :: 冒号，避免 Redis Desktop Manager 多余空格
+        // 详细可见 https://blog.csdn.net/chuixue24/article/details/103928965 博客
+        config = config.computePrefixWith(cacheName -> cacheName + StrUtil.COLON);
+        // 设置使用 JSON 序列化方式
+        config = config.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(buildRedisSerializer()));
+        // 设置 CacheProperties.Redis 的属性
+        CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+        if (redisProperties.getTimeToLive() != null) {
+            config = config.entryTtl(redisProperties.getTimeToLive());
+        }
+        if (redisProperties.getKeyPrefix() != null) {
+            config = config.prefixCacheNameWith(redisProperties.getKeyPrefix());
+        }
+        if (!redisProperties.isCacheNullValues()) {
+            config = config.disableCachingNullValues();
+        }
+        if (!redisProperties.isUseKeyPrefix()) {
+            config = config.disableKeyPrefix();
+        }
+        return config;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisTemplate<String, Object> redisTemplate, RedisCacheConfiguration redisCacheConfiguration) {
+        // 创建 RedisCacheWriter 对象
+        RedisConnectionFactory connectionFactory = Objects.requireNonNull(redisTemplate.getConnectionFactory());
+        RedisCacheWriter cacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+        // 创建 TenantRedisCacheManager 对象
+        return new TimeoutRedisCacheManager(cacheWriter, redisCacheConfiguration);
+    }
+
+}
